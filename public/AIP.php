5e45e58f3c01e1abeffc27153a9524edc2892c29
g020c1073 <108378100+yunosukeT@users.noreply.github.com>
2023-12-14 10:42:43 +0900

    Update AIP.php

--- public/AIP.php
+++ public/AIP.php
@@ -7,21 +7,24 @@
     <link rel="stylesheet" href="./index.css">
     <title>Gallop Genius</title>
 </head>
-<body>
+<body background="./imgFolder/keiba背景_ゴール直前.jpg">
 <div class="wrapper">
     <!--header-->
     <header>
-    <div class="headerText">
-        Gallop Genius
-    </div>
+        <div class="headerText">
+            <a href="./index.php">
+                Gallop Genius
+            </a>
+        </div>
         <div class="hamburger-menu">
             <input type="checkbox" id="menu-btn-check">
             <label for="menu-btn-check" class="menu-btn"><span></span></label>
         <!--menuの中身-->
         <div class="menu-content">
             <ul>
-            <li><a href="./AIP.php">AI Prediction</a></li>
-                <li><a href="./HumanP.php">Human Prediction</a></li>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./AIP.php">Gallop Genius</a></li>
+                <li><a href="./HumanP.php">Nakanohito</a></li>
                 <li><a href="./HorseList.php">Horse List</a></li>
                 <li><a href="./AIP.php">AI Recovery Rate</a></li>
                 <li><a href="./RaceCalendar.php">Race Calendar</a></li>
@@ -34,20 +37,22 @@
     <!--本文-->
     <!--ココから書き換えてください-->
     <div class="mainFrame">
-
+        <div class="titleText">
+            Gallop Genius
+        </div>
     </div>
     </main>
     <!--footer-->
     <footer>
         <div class="footer-menu">
             <ul class="footer-menu-list">
-                <li><img src="./data/keiba_jockey5_yellow.png"></li>
+                <li><img src="./imgFolder/keiba_jockey5_yellow.png"></li>
                 <li>Home</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Home</li>
-                <li><img src="./data/keiba_jockey1_white.png"></li>
+                <li><img src="./imgFolder/keiba_jockey1_white.png"></li>
             </ul>
         </div>
     </footer>
