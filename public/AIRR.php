1329977ea0aff5358e08abafae0c8fcf858aafa4
g020c1073 <108378100+yunosukeT@users.noreply.github.com>
2023-12-14 10:41:21 +0900

    Update AIRR.php

--- public/AIRR.php
+++ public/AIRR.php
@@ -7,19 +7,22 @@
     <link rel="stylesheet" href="./index.css">
     <title>Gallop Genius</title>
 </head>
-<body>
+<body background="./imgFolder/keiba背景4-2.jpg">
 <div class="wrapper">
     <!--header-->
     <header>
-    <div class="headerText">
-        Gallop Genius
-    </div>
+        <div class="headerText">
+            <a href="./index.php">
+                Gallop Genius
+            </a>
+        </div>
         <div class="hamburger-menu">
             <input type="checkbox" id="menu-btn-check">
             <label for="menu-btn-check" class="menu-btn"><span></span></label>
         <!--menuの中身-->
         <div class="menu-content">
             <ul>
+                <li><a href="./index.php">Home</a></li>
                 <li><a href="./AIP.php">AI Prediction</a></li>
                 <li><a href="./HumanP.php">Human Prediction</a></li>
                 <li><a href="./HorseList.php">Horse List</a></li>
@@ -34,20 +37,22 @@
     <!--本文-->
     <!--ココから書き換えてください-->
     <div class="mainFrame">
-
+        <audio autoplay id="kyuin">
+            <source src="./imgFolder./get.mp3">
+        </audio>
     </div>
     </main>
     <!--footer-->
     <footer>
         <div class="footer-menu">
             <ul class="footer-menu-list">
-                <li><img src="./data/keiba_jockey5_yellow.png"></li>
+                <li><img src="./imgFolder/keiba_jockey5_yellow.png"></li>
                 <li>Home</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Home</li>
-                <li><img src="./data/keiba_jockey1_white.png"></li>
+                <li><img src="./imgFolder/keiba_jockey1_white.png"></li>
             </ul>
         </div>
     </footer>
