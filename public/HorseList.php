ec7fef580ac7bf6e8a1adba48743893087adffd8
g020c1073 <108378100+yunosukeT@users.noreply.github.com>
2023-12-14 10:40:34 +0900

    Update HorseList.php

--- public/HorseList.php
+++ public/HorseList.php
@@ -7,19 +7,22 @@
     <link rel="stylesheet" href="./index.css">
     <title>Gallop Genius</title>
 </head>
-<body>
+<body  background="./imgFolder/keiba背景_パドック.jpg">
 <div class="wrapper">
     <!--header-->
     <header>
-    <div class="headerText">
-        Gallop Genius
-    </div>
+        <div class="headerText">
+            <a href="./index.php">
+                Gallop Genius
+            </a>
+        </div>
         <div class="hamburger-menu">
             <input type="checkbox" id="menu-btn-check">
             <label for="menu-btn-check" class="menu-btn"><span></span></label>
         <!--menuの中身-->
         <div class="menu-content">
             <ul>
+                <li><a href="./index.php">Home</a></li>
                 <li><a href="./AIP.php">AI Prediction</a></li>
                 <li><a href="./HumanP.php">Human Prediction</a></li>
                 <li><a href="./HorseList.php">Horse List</a></li>
@@ -31,26 +34,99 @@
         </div>
     </header>
     <main>
+    <!--出馬数を取得-->
+    <?php
+        try{
+            $db = new PDO('mysql:dbname=race;host=localhost;charset=utf8','root','root');
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        $entry = $db->query('SELECT count(*) FROM race.出馬表;');
+    ?>
+    <?php
+        while($count = $entry->fetch()):
+    ?>
+    <?php endwhile?>
     <!--本文-->
     <!--ココから書き換えてください-->
     <div class="mainFrame">
+        <div class="titleText">
+            -Horse List-
+        </div>
+        <div>
+        </div>
+        <div id="horse_count" class="horse_list">
+            <script type="text/javascript">
+                //出馬表の列数を出馬数に合わせる
+                const horse_count = document.getElementById('horse_count');
+                horse_count.innerHTML = '<?php echo $count['count(*)'] ?>';
+            </script>
+                <?php
+                    try{
+                        $db = new PDO('mysql:dbname=race;host=localhost;charset=utf8','root','root');
+                    }catch(PDOException $e){
+                        echo $e->getMessage();
+                    }
+                    $entry = $db->query('SELECT * FROM race.出馬表;');
+                ?>
+                <?php while($resister = $entry->fetch()):?>
+                <div class="horse_data">
+                    <?php if($resister["枠"]=="1"){
+                        echo'<font color="#000" style="background-color:#fff">&nbsp;1&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="2"){
+                        echo'<font color="#fff" style="background-color:#000">&nbsp;2&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="3"){
+                        echo'<font color="#fff" style="background-color:#f00">&nbsp;3&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="4"){
+                        echo'<font color="#fff" style="background-color:#00f">&nbsp;4&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="5"){
+                        echo'<font color="#fff" style="background-color:#ff0">&nbsp;5&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="6"){
+                        echo'<font color="#fff" style="background-color:#0f0">&nbsp;6&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="7"){
+                        echo'<font color="#fff" style="background-color:#ffa500">&nbsp;7&nbsp;</font>';
+                    }?>
+                    <?php if($resister["枠"]=="8"){
+                        echo'<font color="#fff" style="background-color:#f3a0aa">&nbsp;8&nbsp;</font>';
+                    }?>
+                    <?php print($resister["馬番"])?>
+                    <?php print($resister["馬名"])?>
+                    <div class="jockey_name">
+                        <?php print($resister["性齢"])?>
+                        <?php print("　　　　　　")?>
+                        <?php print($resister["騎手"])?>
+                        <?php print($resister["斤量"])?>
+                        <div>__________________________________________</div>
+                    </div>
+                </div>
+            <?php endwhile?>
+            <script>
 
+            </script>
+        </div>
     </div>
     </main>
     <!--footer-->
     <footer>
         <div class="footer-menu">
             <ul class="footer-menu-list">
-                <li><img src="./data/keiba_jockey5_yellow.png"></li>
+                <li><img src="./imgFolder/keiba_jockey5_yellow.png"></li>
                 <li>Home</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Home</li>
-                <li><img src="./data/keiba_jockey1_white.png"></li>
+                <li><img src="./imgFolder/keiba_jockey1_white.png"></li>
             </ul>
         </div>
     </footer>
 </div>
+
 </body>
 </html>
