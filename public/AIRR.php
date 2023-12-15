c3e8ef3e7620a460d33b201e63b999854df5297a
Paseri Y <108378098+g020c1015@users.noreply.github.com>
2023-12-15 09:56:59 +0900

    Update AIRR.php

--- public/AIRR.php
+++ public/AIRR.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="./index.css">
     <title>Gallop Genius</title>
 </head>
-<body background="./imgFolder/keiba背景4-2.jpg">
+<body background="./data/keiba背景4-2.jpg">
 <div class="wrapper">
     <!--header-->
     <header>
@@ -37,22 +37,19 @@
     <!--本文-->
     <!--ココから書き換えてください-->
     <div class="mainFrame">
-        <audio autoplay id="kyuin">
-            <source src="./imgFolder./get.mp3">
-        </audio>
     </div>
     </main>
     <!--footer-->
     <footer>
         <div class="footer-menu">
             <ul class="footer-menu-list">
-                <li><img src="./imgFolder/keiba_jockey5_yellow.png"></li>
+                <li><img src="./data/keiba_jockey5_yellow.png"></li>
                 <li>Home</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Others</li>
                 <li>Home</li>
-                <li><img src="./imgFolder/keiba_jockey1_white.png"></li>
+                <li><img src="./data/keiba_jockey1_white.png"></li>
             </ul>
         </div>
     </footer>
