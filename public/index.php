4dd2f043b7dbd05b446493b8cbcc44d04d858f76
g020c1073 <108378100+yunosukeT@users.noreply.github.com>
2023-12-18 12:28:40 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -36,8 +36,8 @@
     </header>
     <main>
     <!--本文-->
-        <div> </div>
-        <div> </div>
+        <p></p>
+        <p></p>
     <!--ボタン６つ配置-->
         <div class="boxArea">
             <div class="box-AIPrediction">
